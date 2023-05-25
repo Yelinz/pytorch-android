29b38d8458de84ae76af6b45f0fb4013bfb4f8b2
David Schurtenberger <david.schurtenberger@siemens.com>
2023-05-25 16:12:41 +0200

    add more waypoints and find shortest way to waypoints

--- app/src/main/java/org/pytorch/demo/objectdetection/fragment/MapFragment.java
+++ app/src/main/java/org/pytorch/demo/objectdetection/fragment/MapFragment.java
@@ -1,13 +1,25 @@
 package org.pytorch.demo.objectdetection.fragment;
 
+import android.content.ClipData;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.widget.ThemeUtils;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.google.android.material.theme.overlay.MaterialThemeOverlay;
+
 import org.osmdroid.api.IMapController;
+import org.osmdroid.api.IMapView;
 import org.osmdroid.bonuspack.routing.OSRMRoadManager;
 import org.osmdroid.bonuspack.routing.Road;
 import org.osmdroid.bonuspack.routing.RoadManager;
@@ -15,16 +27,31 @@ import org.osmdroid.tileprovider.tilesource.ITileSource;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 import org.pytorch.demo.objectdetection.R;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.prefs.Preferences;
+import java.util.stream.Stream;
 
 
 public class MapFragment extends AbstractMapFragment {
+    private ArrayList<OverlayItem> mapMarkers = new ArrayList<>();
+
     @Override
     public String getSampleTitle() {
         return "Map";
@@ -48,23 +75,47 @@ public class MapFragment extends AbstractMapFragment {
                     mapController.setCenter(mMyLocationOverlay.getMyLocation());
                     mapController.animateTo(mMyLocationOverlay.getMyLocation(), 17d, 900L);
 
-                    // Example route
-                    // needs these 2 lines to work
-                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                    StrictMode.setThreadPolicy(policy);
-                    OSRMRoadManager roadManager = new OSRMRoadManager(requireContext(), "OBP_Tuto/1.0");
-                    roadManager.setMean(OSRMRoadManager.MEAN_BY_FOOT);
-                    ArrayList<GeoPoint> waypoints = new ArrayList<>();
-                    waypoints.add(mMyLocationOverlay.getMyLocation()); // start point
-                    GeoPoint endPoint = new GeoPoint(47.142468, 8.430340);
-                    waypoints.add(endPoint);
-                    Road road = roadManager.getRoad(waypoints);
-                    Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
-                    mMapView.getOverlays().add(roadOverlay);
+                    // add markers
+                    Bundle args = getArguments();
+                    if (args != null) {
+                        String type = args.getString("type");
+                        if (type != null) {
+                            switch (type) {
+                                case "cardboard":
+                                case "aluminum":
+                                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                                    StrictMode.setThreadPolicy(policy);
+                                    GeoPoint myLocation = mMyLocationOverlay.getMyLocation();
+                                    Road road = getShortestRoad(myLocation);
+                                    if (road != null) {
+                                        Polyline roadOverlay = RoadManager.buildRoadOverlay(road, 0x800000FF, 12.5f);
+                                        mMapView.getOverlays().add(roadOverlay);
+                                    }
+                                    break;
+                            }
+                        }
+                    }
                 }
         );
         mMyLocationOverlay.runOnFirstFix(run);
         mMapView.getOverlays().add(mMyLocationOverlay);
+
+
+        this.extractMarkersFromCsvSource();
+        ItemizedIconOverlay<OverlayItem> mOverlay = new ItemizedIconOverlay<>(mapMarkers,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
+                        //do something
+                        return true;
+                    }
+                    @Override
+                    public boolean onItemLongPress(final int index, final OverlayItem item) {
+                        return false;
+                    }
+                }, getActivity());
+
+        mMapView.getOverlays().add(mOverlay);
 
 
         // Example marker at rotkreuz bahnhof
@@ -76,35 +127,34 @@ public class MapFragment extends AbstractMapFragment {
         // startMarker.setIcon(getResources().getDrawable(R.drawable.marker_icon));
         // startMarker.setInfoWindow(new MarkerInfoWindow(R.layout.bonuspack_bubble_black, mMapView));
         mMapView.getOverlays().add(startMarker);
-
-
         mMapView.invalidate();
-
-        // add markers
-        Bundle args = getArguments();
-        if (args == null) {
-            ((LinearLayout) root.findViewById(R.id.mapview)).addView(mMapView);
-            return root;
-        }
-        String type = args.getString("type");
-        if (type == null) {
-            ((LinearLayout) root.findViewById(R.id.mapview)).addView(mMapView);
-            return root;
-        }
-
-        // TODO: mock more markers depending on type
-        switch (type) {
-            case "cardboard":
-            case "aluminum":
-                Marker aluminiumMarker = new Marker(mMapView);
-                aluminiumMarker.setPosition(new GeoPoint(47.143846681, 8.4352684021));
-                aluminiumMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-                aluminiumMarker.setTitle("Recyling Point");
-                mMapView.getOverlays().add(aluminiumMarker);
-                break;
-        }
 
         ((LinearLayout) root.findViewById(R.id.mapview)).addView(mMapView);
         return root;
+    }
+
+    private void extractMarkersFromCsvSource(){
+        try (InputStreamReader inputStream = new InputStreamReader(getResources().getAssets().open("recycling_locations.csv"))){
+            BufferedReader br = new BufferedReader(inputStream);
+            String DELIMITER = ",";
+            String line;
+            while ((line = br.readLine()) != null){
+                String[] columns = line.split(DELIMITER);
+                mapMarkers.add(new OverlayItem(columns[0], columns[1], new GeoPoint(Double.parseDouble(columns[2]), Double.parseDouble(columns[3]))));
+            }
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private Road getShortestRoad(GeoPoint currentLocation){
+        ArrayList<GeoPoint> waypoints = new ArrayList<>();
+        OSRMRoadManager roadManager = new OSRMRoadManager(requireContext(), "OBP_Tuto/1.0");
+        roadManager.setMean(OSRMRoadManager.MEAN_BY_FOOT);
+        return mapMarkers.stream()
+                .map(i -> (GeoPoint) i.getPoint())
+                .map(i -> roadManager.getRoad(new ArrayList<GeoPoint>(Arrays.asList(currentLocation, i))))
+                .min(Comparator.comparingDouble(i -> i.mDuration))
+                .orElse(null);
     }
 }
